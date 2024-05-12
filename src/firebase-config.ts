43064f6922634c6c00e668cb80fac386823a2e83
Busraltun258 <altunbusra32@gmail.com>
2024-05-12 23:19:57 +0300

    fix

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,6 +1,6 @@
+"use client";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,7 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 
